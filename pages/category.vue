943cfd8a6baebdb6f98c76e4116291d0b4701081
Victor Otávio Ferreira <victor@ferreira.id>
2023-06-04 16:29:07 -0300

    feat: Form templates

---
+++ pages/category.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Cadastrar categoria</h1>
+
+    <form>
+      <div>
+        <label for="name">Nome:</label>
+        <input type="text" name="name" />
+      </div>
+
+      <div>
+        <label><input type="radio" />Despesa</label>
+        <label><input type="radio" />Receita</label>
+      </div>
+
+      <button type="submit">Cadastrar</button>
+    </form>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>Cadastrar despesa</h1>
+
+    <form>
+      <div>
+        <label for="value">Valor:</label>
+        <input type="number" name="value" />
+      </div>
+
+      <div>
+        <label for="category">Catergoria:</label>
+        <select name="category">
+          <option value="">Selecione...</option>
+        </select>
+      </div>
+
+      <div>
+        <label for="description">Descrição:</label>
+        <textarea name="description"></textarea>
+      </div>
+
+      <button type="submit">Cadastrar</button>
+    </form>
+  </div>
+</template>
