b667c265f436bfd97617759f388da3cba560a115
Victor Otávio Ferreira <victor@ferreira.id>
2024-03-09 15:48:15 -0300

    chore: install NuxtUI

--- app.vue
+++ app.vue
@@ -1,5 +1,18 @@
 <template>
-  <div>
-    <NuxtPage />
-  </div>
+  <Html lang="pt-BR">
+    <Head>
+      <Title>Billing System</Title>
+      <Meta name="description" content="Controle financeiro" />
+    </Head>
+
+    <Body
+      class="relative z-0 bg-zinc-100 text-base font-normal text-zinc-700 antialiased dark:bg-zinc-900 dark:text-zinc-200"
+    >
+      <NuxtLayout>
+        <NuxtPage />
+      </NuxtLayout>
+
+      <UNotifications />
+    </Body>
+  </Html>
 </template>
---
+++ layouts/default.vue
@@ -0,0 +1,5 @@
+<template>
+  <main class="container mx-auto py-4 md:py-6 2xl:py-10">
+    <slot />
+  </main>
+</template>
