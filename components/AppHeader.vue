d3bb380b087846efa96c65df9a88a428bb17e7d5
Victor Otávio Ferreira <victor@ferreira.id>
2024-03-11 10:26:46 -0300

    feat: add app header

---
+++ components/AppHeader.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup></script>
+
+<template>
+  <header
+    class="sticky top-0 z-40 -mb-px flex w-full border-b border-gray-200 bg-white/75 backdrop-blur dark:border-gray-800 dark:bg-zinc-800/75"
+  >
+    <div
+      class="flex flex-grow items-center justify-between px-4 py-4 drop-shadow-sm md:px-6 2xl:px-11"
+    >
+      <div class="flex items-center gap-2 sm:gap-4">
+        <NuxtLink class="block flex-shrink-0" to="/">
+          <UIcon name="i-heroicons-currency-dollar" class="size-4" />
+        </NuxtLink>
+      </div>
+
+      <div class="flex items-center gap-3 sm:gap-7">
+        <ul class="flex items-center gap-2 sm:gap-4">
+          <li>
+            <NuxtLink to="/">Transações</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/categories">Categorias</NuxtLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </header>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,13 @@
 <template>
-  <main class="container mx-auto py-4 md:py-6 2xl:py-10">
-    <slot />
-  </main>
+  <div class="flex h-screen overflow-hidden">
+    <div class="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
+      <AppHeader />
+
+      <main>
+        <div class="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
+          <slot />
+        </div>
+      </main>
+    </div>
+  </div>
 </template>
